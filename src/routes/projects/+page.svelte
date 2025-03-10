0386dbfbcdbf544604cd1c9930314eec0560f613
Anthony Graignic <12660783+anthonygraignic@users.noreply.github.com>
2025-03-10 22:01:54 +0100

    Add cards to projects page

--- src/routes/projects/+page.svelte
+++ src/routes/projects/+page.svelte
@@ -1,7 +1,3 @@
-<script>
-	import ExternalLink from '$lib/components/ExternalLink.svelte';
-</script>
-
 <svelte:head>
 	<title>Projects | Anthony Graignic</title>
 	<meta name="description" content="A partial list of projects by Anthony Graignic" />
@@ -10,11 +6,27 @@
 	<div class="space-y-4">
 		<p class="text-center">Here you will find some of my solo projects:</p>
 
-		<p>
-			<ExternalLink url="https://themergetrees.xyz">The Merge Trees 🌳</ExternalLink>
-			, an on-chain NFT collection questioning the relationship between humans and ecology.
-		</p>
-
-		<a href="/photography" class="underline">My Photography Portfolio 📸</a>
+		<div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+			<a
+				href="/photography"
+				class="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-sm hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+			>
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					My Photo Portfolio 📸
+				</h5>
+				<p class="font-normal text-gray-700 dark:text-gray-400">Moments from here and there.</p>
+			</a>
+			<a
+				href="https://themergetrees.xyz"
+				class="block max-w-sm rounded-lg border border-gray-200 bg-white p-6 shadow-sm hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-800 dark:hover:bg-gray-700"
+			>
+				<h5 class="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+					The Merge Trees 🌳
+				</h5>
+				<p class="font-normal text-gray-700 dark:text-gray-400">
+					An on-chain NFT collection questioning the relationship between humans and ecology.
+				</p>
+			</a>
+		</div>
 	</div>
 </main>
