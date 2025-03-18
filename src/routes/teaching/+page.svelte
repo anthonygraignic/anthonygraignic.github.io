1b2dd340fd15c84892150af3e75a79fd225b448d
Anthony Graignic <12660783+anthonygraignic@users.noreply.github.com>
2025-03-18 22:06:55 +0100

    Translate teaching in english

--- src/routes/teaching/+page.svelte
+++ src/routes/teaching/+page.svelte
@@ -7,38 +7,42 @@
 	<meta name="description" content="Teaching and mentoring done by Anthony Graignic" />
 </svelte:head>
 
-<main class="flex flex-col items-center justify-center justify-items-center p-4 font-mono">
+<main
+	class="mx-auto flex max-w-5xl flex-col items-center justify-center justify-items-center p-4 font-mono"
+>
 	<div class="space-y-4">
 		<p>
-			Mentor
-			<ExternalLink url="https://inge-plus.fr/">Ingé+</ExternalLink> depuis 2024
+			Mentor for
+			<ExternalLink url="https://inge-plus.fr/">Ingé+</ExternalLink> since 2024, inspiring bachelor students
+			to become engineers
 		</p>
 
 		<p>
-			Masterclass à l'<ExternalLink url="https://em-lyon.com/">EM Lyon</ExternalLink> sur <ExternalLink
+			Masterclass at
+			<ExternalLink url="https://em-lyon.com/">EM Lyon</ExternalLink> on <ExternalLink
 				url="https://ecomobicoin.limos.fr/">EcoMobiCoin</ExternalLink
-			> en 2025
+			> in 2025
 		</p>
 		<p>
-			Formateur sur le module <ExternalLink
+			Teacher of the <ExternalLink
 				url="https://www.ecole-hexagone.com/fr/fr/cursus/intelligence-artificielle/#program"
 				>"Blockchain"</ExternalLink
-			> à l'École Hexagone en 2024
+			>
+			module at École Hexagone in 2024
 		</p>
 		<p>
-			Speaker de multiples fois à <ExternalLink url="https://www.clermontech.org/talks/"
+			Numerous talks at <ExternalLink url="https://www.clermontech.org/talks/"
 				>Clermont'ech</ExternalLink
 			>
 		</p>
 		<p>
-			Mentor <ExternalLink url="https://openclassrooms.com/fr/paths/749-architecte-logiciel"
+			Mentor at <ExternalLink url="https://openclassrooms.com/fr/paths/749-architecte-logiciel"
 				>OpenClassrooms</ExternalLink
-			> sur le parcours "Architecte Logiciel" (RNCP niveau 7, bac+5) de 2020 à 2024
+			> on the "Software Architect" training program (RNCP level 7, bac+5) from 2020 to 2024
 		</p>
 		<p>
-			Bénévole dans l'association <ExternalLink url="https://www.girlcode.co.za/"
-				>Girlcode ZA</ExternalLink
-			> en Afrique du Sud de 2020 à 2022
+			Volunteer for the <ExternalLink url="https://www.girlcode.co.za/">Girlcode ZA</ExternalLink> NGO
+			(aimed at empowering young girls and women through technology) in South Africa from 2020 to 2022
 		</p>
 	</div>
 </main>
